c114d52f1318b7f7387d09c1478d8332314f11f0
AmarjeetMahato <amarjitmahato016@gmail.com>
2026-05-18 00:39:01 +0530

    complete the admin controllers

--- src/main/java/com/schoolManagementDB/domain/Address/controllers/AddressController.java
+++ src/main/java/com/schoolManagementDB/domain/Address/controllers/AddressController.java
@@ -1,9 +1,10 @@
 package com.schoolManagementDB.domain.Address.controllers;
 
 
-import com.schoolManagementDB.dtos.AddressDto;
-import com.schoolManagementDB.entities.Address;
-import com.schoolManagementDB.services.AddressService.AddressService;
+import com.schoolManagementDB.domain.Address.dtos.AddressDto;
+import com.schoolManagementDB.domain.Address.dtos.AddressResponseDto;
+import com.schoolManagementDB.domain.Address.dtos.AddressUpdateDto;
+import com.schoolManagementDB.domain.Address.services.IAddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,36 +18,36 @@ import java.util.List;
 public class AddressController {
 
 
-    private final AddressService addressService;
+    private final IAddressService addressService;
 
     // Create address
     @PostMapping("/create")
-    public ResponseEntity<Address> createAddress(@RequestBody AddressDto addressDto) {
-        Address address = addressService.createAddress(addressDto);
+    public ResponseEntity<AddressResponseDto> createAddress(@RequestBody AddressDto addressDto) {
+        AddressResponseDto address = addressService.createAddress(addressDto);
         return ResponseEntity.ok(address);
     }
 
     // Get address by ID
     @GetMapping("/{id}")
-    public ResponseEntity<Address> getAddressById(@PathVariable("id") String id) {
-        Address address = addressService.getAddressById(id);
+    public ResponseEntity<AddressResponseDto> getAddressById(@PathVariable("id") String id) {
+        AddressResponseDto address = addressService.getAddressById(id);
         return ResponseEntity.ok(address);
     }
 
     // Get all addresses
     @GetMapping
-    public ResponseEntity<List<AddressDto>> getAllAddresses() {
-        List<AddressDto> addresses = addressService.getAllAddresses();
+    public ResponseEntity<List<AddressResponseDto>> getAllAddresses() {
+        List<AddressResponseDto> addresses = addressService.getAllAddresses();
         return ResponseEntity.status(HttpStatus.OK).body(addresses);
     }
 
     // Update address
     @PutMapping("/{id}")
-    public ResponseEntity<Address> updateAddress(
+    public ResponseEntity<AddressResponseDto> updateAddress(
             @PathVariable("id") String id,
-            @RequestBody AddressDto addressDto
+            @RequestBody AddressUpdateDto addressDto
     ) {
-        Address updatedAddress = addressService.updateAddress(id, addressDto);
+        AddressResponseDto updatedAddress = addressService.updateAddress(id, addressDto);
         return ResponseEntity.ok(updatedAddress);
     }
 
@@ -59,19 +60,19 @@ public class AddressController {
 
     // Filter by city
     @GetMapping("/city/{city}")
-    public ResponseEntity<List<AddressDto>> getAddressesByCity(@PathVariable("city") String city) {
+    public ResponseEntity<List<AddressResponseDto>> getAddressesByCity(@PathVariable("city") String city) {
         return ResponseEntity.ok(addressService.getAddressesByCity(city));
     }
 
     // Filter by state
     @GetMapping("/state/{state}")
-    public ResponseEntity<List<AddressDto>> getAddressesByState(@PathVariable("state") String state) {
+    public ResponseEntity<List<AddressResponseDto>> getAddressesByState(@PathVariable("state") String state) {
         return ResponseEntity.ok(addressService.getAddressesByState(state));
     }
 
     // Filter by postal code
     @GetMapping("/postal-code/{postalCode}")
-    public ResponseEntity<List<AddressDto>> getAddressesByPostalCode(@PathVariable("postalCode") String postalCode) {
+    public ResponseEntity<List<AddressResponseDto>> getAddressesByPostalCode(@PathVariable("postalCode") String postalCode) {
         return ResponseEntity.ok(addressService.getAddressesByPostalCode(postalCode));
     }
 }
